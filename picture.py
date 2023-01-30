80003228bf9166e36ed75f67cd462192c22c9c4b
neonellka <106733091+neonellka@users.noreply.github.com>
2023-01-30 13:07:04 +0300

    сделаль облака

--- picture.py
+++ picture.py
@@ -1,8 +1,38 @@
 import turtle
+ 
+turtlePen = turtle.Turtle()
+window = turtle.Screen()
+ 
+ 
+def draw_stairs(n):
+    turtlePen.color("blue")
+    for i in range(0, n):
+        turtlePen.forward(150)
+        turtlePen.left(90)
+        turtlePen.forward(50)
+        turtlePen.left(90)
+        turtlePen.forward(50)
+        turtlePen.right(90)
+        turtlePen.forward(50)
+        turtlePen.left(90)
+        turtlePen.forward(50)
+        turtlePen.left(90)
+        turtlePen.forward(50)
+        turtlePen.right(90)
+        turtlePen.forward(50)
+        turtlePen.left(90)
+        turtlePen.forward(50)
 
-def sun():
-  """
-  Нелля рисует тучки
-  """
-  
-  return None
+turtlePen.speed(10)
+
+
+draw_stairs(1)   # &lt;- вызов функции
+ 
+turtlePen.color("white")
+turtlePen.right(90)
+turtlePen.forward(250)
+turtlePen.right(180)
+
+draw_stairs(1)
+
+window.mainloop() 
